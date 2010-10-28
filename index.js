f528bb3dfaf5f8e76cdc8fc0f3516671fb0c2c5a
Alex <maccman@gmail.com>
2010-10-28 09:54:49 +0100

    complete vendorize

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-require.paths.unshift("./vendor");
+require("fs").readdirSync("./vendor").forEach(function(name){
+  require.paths.unshift("./vendor/" + name + "/lib");  
+});
+
 require.paths.unshift("./lib");
 module.exports = require("./lib/juggernaut");
+
